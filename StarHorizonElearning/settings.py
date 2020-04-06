f42f64fce9ed383119cbcc89ad7975508e21e7db
Yongtheskill <yongee2003@gmail.com>
2020-04-06 22:08:53 +0800

    home page and base css

--- StarHorizonElearning/settings.py
+++ StarHorizonElearning/settings.py
@@ -25,12 +25,13 @@ SECRET_KEY = 'u!t(kq+5a3ofa9az#q!p0%kp%zuhv(tsvtf&9b&c58zh)q6cak'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'home.apps.HomeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'StarHorizonElearning.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    'static/',
+]
---
+++ home/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+
+import datetime
+import pytz
+from django.utils import timezone
+
+
+#home page
+def home(request):
+    context = {}
+    return render(request, 'home/home.html', context)
