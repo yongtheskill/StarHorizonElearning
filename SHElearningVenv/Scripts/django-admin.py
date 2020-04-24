cfabd3c9a36278088ed318fea8d05a654ad6dc77
Yongtheskill <yongee2003@gmail.com>
2020-04-25 00:16:46 +0800

    add virtualenv

---
+++ SHElearningVenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!E:\Local Disk\Desktop\StarHorizonElearning\SHElearningVenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
