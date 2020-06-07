e46a2b08c087d66cf18cfbb6c81b8429ed8cdd47
Yongtheskill <yongee2003@gmail.com>
2020-06-07 16:01:35 +0800

    QUIZ PREVIEW AND EDIT!!!!!!

--- quizzes/urls.py
+++ quizzes/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('manage/', views.manageQuizzes, name='manageQuizzes'),
     path('create/', views.createQuiz, name='createQuizzes'),
+    path('view/<str:quizID>/', views.viewQuiz, name='viewQuiz'),
+    path('edit/<str:quizID>/', views.editQuiz, name='editQuiz'),
 ]
--- quizzes/views.py
+++ quizzes/views.py
@@ -55,3 +55,45 @@ def createQuiz(request):
 
     else:
         return render(request, 'quizzes/create.html', context)
+
+
+
+#quiz view page
+@login_required
+def viewQuiz(request, quizID):
+
+    context = {"quizObject": Quiz.objects.get(quizID=quizID), }
+
+    return render(request, 'quizzes/view.html', context)
+    
+
+
+
+
+#quiz edit page
+@login_required
+def editQuiz(request, quizID):
+    
+    #if submitting form
+    if request.method == 'POST':
+        questionsJSON = request.POST['allQuestionsJSON']
+        questionsJSON = re.sub("_____var__", "", questionsJSON) #remove js stuff
+
+        #find matching course
+        assignedCourseID = request.POST["assignedCourse"]
+        assignedCourse = Course.objects.get(pk=assignedCourseID)
+
+        newQuiz = Quiz.objects.get(quizID=request.POST['quizID'])
+        newQuiz.quizName = request.POST['quizName']
+        newQuiz.course = assignedCourse
+        newQuiz.quizData = questionsJSON
+        newQuiz.save()
+        
+        context = {"quizObjects": Quiz.objects.all, "notification": "Successfully edited quiz", }
+        return render(request, 'quizzes/manage.html', context)
+
+
+    else:
+        context = {"courseObjects": Course.objects.all, "quizObject": Quiz.objects.get(quizID=quizID), }
+        return render(request, 'quizzes/edit.html', context)
+    
